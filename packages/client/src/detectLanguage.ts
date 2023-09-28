c5c3d5f220039b22a4734f0f2907d7bd0d6cdb99
Jason Johnston <jjohnston@matterport.com>
2023-09-28 15:42:07 -0700

    fix: better lang detection using regex unicode script classes

--- packages/client/src/detectLanguage.ts
+++ packages/client/src/detectLanguage.ts
@@ -3,8 +3,8 @@
  * https://gist.github.com/ceshine/1f8cd81ce34d89f1429d0928c28d97e4#file-detector-py
  */
 export function detectLanguage(text: string): string {
-  if (/[\uac00-\ud7a3]/.test(text)) return 'ko'
-  if (/[\u3040-\u30ff]/.test(text)) return 'ja'
+  if (/\p{Script=Hangul}/u.test(text)) return 'ko'
+  if (/\p{Script=Hiragana}|\p{Script=Katakana}/u.test(text)) return 'ja'
   // zh-Hant?
   return 'en'
 }
